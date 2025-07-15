0c1145abefff80e3502de049e7c8996de48bfeb7
leefshitz2 <leefshitz2@gmail.com>
2025-07-15 18:20:49 +0300

    TWRP

--- twrp_a06.mk
+++ twrp_a06.mk
@@ -1,18 +1,15 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator#
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from a06 device
+# Inherit from a05m device
 $(call inherit-product, device/samsung/a06/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
