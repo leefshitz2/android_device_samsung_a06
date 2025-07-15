93ad814cfc3bcbcf8751a1a439f93f17f840a5f9
leefshitz2 <leefshitz2@gmail.com>
2025-07-15 08:05:24 +0300

    TWRP

--- twrp_a06.mk
+++ twrp_a06.mk
@@ -1,14 +1,17 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from a05m device
+# Inherit from a06 device
 $(call inherit-product, device/samsung/a06/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -23,4 +26,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="a06xx-user 12 SP1A.210812.016 A065FXXS3AYB1 release-keys"
+
 BUILD_FINGERPRINT := samsung/a06xx/a06:12/SP1A.210812.016/A065FXXS3AYB1:user/release-keys
